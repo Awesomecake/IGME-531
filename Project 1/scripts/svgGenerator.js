0571a9401c2fb06de6a5988c37b1ae46dff80f4f
Aaron Bush (RIT Student) <aob6548@rit.edu>
2024-02-20 10:18:49 -0500

    tweaked project 1

--- Project 1/scripts/svgGenerator.js
+++ Project 1/scripts/svgGenerator.js
@@ -70,6 +70,22 @@ const generateTruchet = (offsetX, offsetY,width,height,strokewidth,strokeColor="
     return createGrouping(output, {rotation: rng(2)*90, translation: [offsetX*width,offsetY*height]});
 }
 
+const generateTripleCurve = (offsetX, offsetY,width,height,strokewidth,strokeColor="green") => {
+    //Line One
+    let output = `<path fill="none" stroke="${strokeColor}" stroke-width="${strokewidth}" 
+        d="M${-width/2},0 a${width/2},${height/2} 0 0,1 ${width/2},${height/2}" />`
+        
+    //Line Two
+    output += `<path fill="none" stroke="${strokeColor}" stroke-width="${strokewidth}" 
+        d="M0,${height/2} a${width/2},${height/2} 0 0,1 ${width/2},${-height/2}" />`
+    
+        //Line Three
+    output += `<path fill="none" stroke="${strokeColor}" stroke-width="${strokewidth}" 
+        d="M${width/2},0 a${width/2},${height/2} 0 0,1 ${-width/2},${-height/2}" />`
+
+    return createGrouping(output, {rotation: rng(2)*90, translation: [offsetX*width,offsetY*height]});
+}
+
 const createGrouping = (svg, {rotation = 0, translation = [0,0], pivot = [0,0], scale = [1,1]} ) => {
     return `<g transform=" translate(${translation[0]},${translation[1]}) rotate(${rotation},${pivot[0]},${pivot[1]})scale(${scale[0]},${scale[1]})"> ${svg} </g>`;
 }
@@ -78,48 +94,24 @@ const rng = (max) => { return Math.floor(Math.random() * max); }
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
-generateTiling('#svgContainer1', [generateCircle, generateTruchet],20,20,80);
-generateTiling('#svgContainer2', [generateTruchet], 20,20,80);
-generateTiling('#svgContainer3', [generateDiagonal], 20,20,80);
-generateTiling('#svgContainer4', [generateTruchet,generateDiagonal, generateCross,generateArrow, generateCircle], 20,20,80);
-generateTiling('#svgContainer5', [generateCross], 20,20,80);
-generateTiling('#svgContainer6', [generateCross,generateTruchet], 20,20,80);
+generateTiling('#svgContainer1', [generateCircle, generateTruchet],16,16,100);
+generateTiling('#svgContainer2', [generateTruchet],16,16,100);
+generateTiling('#svgContainer3', [generateDiagonal],16,16,100);
+generateTiling('#svgContainer4', [generateTruchet,generateDiagonal, generateCross,generateArrow, generateCircle],16,16,100);
+generateTiling('#svgContainer5', [generateCross],16,16,100);
+generateTiling('#svgContainer6', [generateCross,generateTruchet],16,16,100);
+generateTiling('#svgContainer7', [generateTripleCurve],16,16,100);
+generateTiling('#svgContainer8', [generateDiagonal,generateTruchet],16,16,100);
 
+// const tilingOptions = [generateCross,generateDiagonal,generateArrow,generateTruchet]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Test Curve
-////////////////////////////////////////////////////////////////////////////////////////
-// const generateCurve = (offsetX, offsetY,width,height,strokewidth) => {
-//     let svg = `<path fill="none" stroke="red" stroke-width="${strokewidth}" 
-//     d="M${width},${height} 
-//     a${width},${height} 0 
-//     0,1 
-//     -${width},-${height}" />`
-
-//     switch(rng(4))
+// for(let i = 1; i <= 8; i++)
+// {
+//     let tilingTypes = []
+//     for(let j = 0; j < rng(tilingOptions.length)+1; j++)
 //     {
-//         case 0:
-//             return createGrouping(svg, {rotation: 0, translation: [offsetX*width,offsetY*height], pivot: [width/2,height/2]});
-//         case 1:
-//             return createGrouping(svg, {rotation: 90, translation: [offsetX*width,offsetY*height], pivot: [width/2,height/2]});
-//         case 2:
-//             return createGrouping(svg, {rotation: 180, translation: [offsetX*width,offsetY*height], pivot: [width/2,height/2]});
-//         case 3:
-//             return createGrouping(svg, {rotation: 270, translation: [offsetX*width,offsetY*height], pivot: [width/2,height/2]});
+//         tilingTypes.push(tilingOptions[rng(tilingOptions.length)]);
 //     }
+//     console.log(tilingTypes);
+//     generateTiling(`#svgContainer${i}`,tilingTypes,16,16,100);
 // }
